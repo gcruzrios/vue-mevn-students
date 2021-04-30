59df388efcab3ae08c671523696a8b7dda8e2166
gcruzrios <gcruzr@gmail.com>
2021-04-30 15:17:12 -0600

    backend con BD MongoAtlas

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-   db: 'mongodb://localhost:27017/vuecrudmevn'
+   db: 'mongodb+srv://gcruzrios:Grvn240675@cluster0.jbim8.mongodb.net/vuecrudmevn?retryWrites=true&w=majority'
+ //db: 'mongodb://localhost:27017/vuecrudmevn'
 }
